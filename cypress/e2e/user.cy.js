7dc38762f88107dcaf17dc056c739306bbe7d9b0
OlgaCh <Chamina51@mail.ru>
2024-06-21 11:39:43 +0300

    next commit

--- cypress/e2e/user.cy.js
+++ cypress/e2e/user.cy.js
@@ -11,19 +11,33 @@ describe('template spec', () => {
             userStatus: 0
         }).then((response) => {
             expect(response.status).be.eql(200);
-            expect(response.body).be.eql({
-                id: 0,
-                username: 'Ann',
-                firstName: 'Anna',
-                lastName: 'Li',
-                email: 'AnnaLi@mail.ru',
-                password: '1234',
-                phone: '8910234567',
-                userStatus: 0
-            });
         });
-        // cy.request('DELETE', `user/${response.body.id}`).then((response) => {
-        //     expect(response.status).be.eql(200);
-        // });
     });
+    it('должен изменять пользователя', () => {
+        const updatedUser = {
+            id: 0,
+            username: 'Anton',
+            firstName: 'Anton',
+            lastName: 'second',
+            email: 'updatedtestcy@example.com',
+            password: 'updatedtestcypassword',
+            phone: '0987654321',
+            userStatus: 1
+        };
+
+        cy.request({
+            method: 'PUT',
+            url: `${baseUrl}/user/Ann`,
+            body: updatedUser,
+            headers: {
+                'Content-Type': 'application/json',
+                accept: 'application/json'
+            }
+        })
+            .its('status')
+            .should('eq', 200);
+    });
+    //cy.request('DELETE', `user/${response.body.id}`).then((response) => {
+    //    expect(response.status).be.eql(200);
+    // });
 });
