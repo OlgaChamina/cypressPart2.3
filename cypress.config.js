d51834c70fb114c70ab9e9a006dbbf21b9f347e1
OlgaCh <Chamina51@mail.ru>
2024-06-30 19:22:58 +0300

    next commit

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
     e2e: {
-        baseUrl: 'https://petstore.swagger.io/v2/',
+        baseUrl: 'https://petstore.swagger.io/v2/user',
         setupNodeEvents(on, config) {
             // implement node event listeners here
         }
--- cypress/e2e/user.cy.js
+++ cypress/e2e/user.cy.js
@@ -1,14 +1,18 @@
 describe('template spec', () => {
     it('создание пользователя', () => {
-        cy.request('POST', 'user', {
-            id: 0,
-            username: 'Ann',
-            firstName: 'Anna',
-            lastName: 'Li',
-            email: 'AnnaLi@mail.ru',
-            password: '1234',
-            phone: '8910234567',
-            userStatus: 0
+        cy.request({
+            method: 'POST',
+            url: '/',
+            body: {
+                id: 0,
+                username: 'Ann',
+                firstName: 'Anna',
+                lastName: 'Li',
+                email: 'AnnaLi@mail.ru',
+                password: '1234',
+                phone: '8910234567',
+                userStatus: 0
+            }
         }).then((response) => {
             expect(response.status).be.eql(200);
         });
@@ -20,14 +24,14 @@ describe('template spec', () => {
             firstName: 'Anton',
             lastName: 'second',
             email: 'updatedtestcy@example.com',
-            password: 'updatedtestcypassword',
+            password: '123',
             phone: '0987654321',
             userStatus: 1
         };
 
         cy.request({
             method: 'PUT',
-            url: `${baseUrl}/user/Ann`,
+            url: `https://petstore.swagger.io/v2/user/Ann`,
             body: updatedUser,
             headers: {
                 'Content-Type': 'application/json',
@@ -37,7 +41,12 @@ describe('template spec', () => {
             .its('status')
             .should('eq', 200);
     });
-    //cy.request('DELETE', `user/${response.body.id}`).then((response) => {
-    //    expect(response.status).be.eql(200);
-    // });
+    it('должен удалять пользователя', () => {
+        cy.request({
+            method: 'DELETE',
+            url: `https://petstore.swagger.io/v2/user/Ann`
+        }).then((response) => {
+            expect(response.status).be.eql(200);
+        });
+    });
 });
